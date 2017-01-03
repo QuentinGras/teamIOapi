792f53805e2ef50a0448fb45b5aba2174d575a4d
Quentin Gras <gras_q@epita.fr>
2017-01-03 15:44:39 +0100

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   $method = $_SERVER['REQUEST_METHOD'];
-  $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+  $request = explode('/', trim($_SERVER['REQUEST_URI'],'/'));
   $input = json_decode(file_get_contents('php://input'),true);
 
   print_r($request);
