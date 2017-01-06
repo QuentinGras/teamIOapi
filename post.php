79cec7ff68c6aad02ca4c3049b6fd5a02f8fb3ae
Quentin Gras <gras_q@epita.fr>
2017-01-06 11:24:57 +0100

    update

--- post.php
+++ post.php
@@ -3,7 +3,8 @@
   $data = explode('?', trim($request[2],'?'));
   //TODO: Gerer le remplissage de la BDD
 //INSERT INTO `docapostnwdocadb`.`WaWpause` VALUES ()
-  //  $input = json_decode(file_get_contents('php://input'),true);
+  $input = json_decode(file_get_contents('php://input'),true);
+  print_r($input);
   try
   {
       $bdd = new PDO('mysql:host=docapostnwdocadb.mysql.db;dbname=docapostnwdocadb;', 'docapostnwdocadb', 'Doca2016');
@@ -14,6 +15,7 @@
   }
   switch ($data[0]) {
     case 'temp':
+      if()
       $req = $bdd->prepare('INSERT INTO WaWTemp(value) VALUES(?)');
       $req->execute(array($data[1]));
       break;
